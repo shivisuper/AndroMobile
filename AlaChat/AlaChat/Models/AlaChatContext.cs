81d164540f1b1af1c07af07c1459248997c3eb7c
Umer Altaf <mumeraltaf@hotmail.com>
2016-10-04 00:20:31 +1100

    Attached DB and Added first controller

---
+++ AlaChat/AlaChat/Models/AlaChatContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AlaChat.Models
+{
+    public class AlaChatContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public AlaChatContext() : base("name=AlaChatContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<AlaChat.ala_user> ala_user { get; set; }
+    }
+}
