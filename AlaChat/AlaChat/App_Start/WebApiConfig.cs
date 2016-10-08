65a600bd49e10a514360ff549a1be8b1269d45e0
Umer Altaf <mumeraltaf@hotmail.com>
2016-10-09 01:09:23 +1100

    Connected Controller to DB and tested on AWS, server is a go for functionality

--- AlaChat/AlaChat/App_Start/WebApiConfig.cs
+++ AlaChat/AlaChat/App_Start/WebApiConfig.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Web.Http;
+﻿using System.Web.Http;
 using Microsoft.Owin.Security.OAuth;
-using Newtonsoft.Json.Serialization;
-
+using System.Web.Http.OData.Builder;
 namespace AlaChat
 {
     public static class WebApiConfig
@@ -20,11 +15,20 @@ namespace AlaChat
             // Web API routes
             config.MapHttpAttributeRoutes();
 
+          //  config.Routes.MapHttpRoute(
+          //      name: "DefaultApi",
+          //      routeTemplate: "api/{controller}/{id}",
+          //      defaults: new { id = RouteParameter.Optional }
+          //  );
             config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
+             name: "DefaultApi",
+             routeTemplate: "api/{controller}/{action}/{id}",
+             defaults: new { id = RouteParameter.Optional }
+         );
+
+
+
+
         }
     }
 }
